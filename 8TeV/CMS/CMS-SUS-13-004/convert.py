f8ef24d4d739247cf701124a902f2f23e847214c
Federico Ambrogi <Federico.Ambrogi@oeaw.ac.at>
2015-02-10 18:10:49 +0100

    Added New TxNames

--- 8TeV/CMS/CMS-SUS-13-004/convert.py
+++ 8TeV/CMS/CMS-SUS-13-004/convert.py
@@ -31,7 +31,7 @@ info.implemented_by = 'Suchita Kulkarni'
 T1bbbb = TxName('T1bbbb')
 #T1bbbb.on.checked =
 #T1bbbb.off.checked =
-T1bbbb.on.constraint = "[[['b','b']]]"
+T1bbbb.on.constraint = "[[['b','b']],[['b','b']]]"
 #T1bbbb.off.constraint =
 T1bbbb.on.condition = None
 #T1bbbb.off.condition =
@@ -41,7 +41,7 @@ T1bbbb.off.fuzzycondition = None
 #+++++++ next mass plane block ++++++++++++++
 T1bbbb_1 = T1bbbb.addMassPlane(motherMass = x, lspMass = y)
 #----figure----
-T1bbbb_1.figure = 'T1bbbbRazorXSEC.pdf'
+T1bbbb_1.figure = 'Figure 12'
 T1bbbb_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T1bbbbRazorXSEC.pdf'
 #----limit source----
 T1bbbb_1.limit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = "xsecUL_Obs_T1bbbb_MultiJet_Jet2b")
@@ -69,85 +69,85 @@ T1bbbb_1.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS1
 #T1bbbb_1.expectedExclusionP1.dataUrl =
 
 #+++++++ next txName block ++++++++++++++
-'''T2tt = TxName('T2tt')
-T2tt.on.checked =
-T2tt.off.checked =
-T2tt.on.constraint =
-T2tt.off.constraint =
-T2tt.on.condition =
-T2tt.off.condition =
-T2tt.on.fuzzycondition =
-T2tt.off.fuzzycondition =
+T2tt = TxName('T2tt')
+#T2tt.on.checked =
+#T2tt.off.checked =
+T2tt.on.constraint = "[[['t']],[['t']]]"
+T2tt.off.constraint = "[[['b','W']],[['b','W']]]"
+T2tt.on.condition = None
+T2tt.off.condition = None
+T2tt.on.fuzzycondition = None
+T2tt.off.fuzzycondition = None
 
 #+++++++ next mass plane block ++++++++++++++
-T2tt_1 = T2tt.setMassPlane(motherMass = , lspMass = )
+T2tt_1 = T2tt.addMassPlane(motherMass = x, lspMass = y )
 #----figure----
-T2tt_1.figure =
-T2tt_1.figureUrl =
+T2tt_1.figure = 'Figure 13'
+T2tt_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T1ttttRazorXSEC.pdf'
 #----limit source----
-T2tt_1.limit.setSource( path, type, object = None, index = None )
-T2tt_1.expectedlimit.setSource( path, type, object = None, index = None )
+T2tt_1.limit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Obs_T2tt_MultiJet_Jet2b_MuJet', index = None )
+T2tt_1.expectedlimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Exp_T2tt_MultiJet_Jet2b_MuJet', index = None )
 #----exclusion source----
-T2tt_1.exclusion.setSource( path, type, object = None, index = None )
-T2tt_1.exclusionM1.setSource( path, type, object = None, index = None )
-T2tt_1.exclusionP1.setSource( path, type, object = None, index = None )
-T2tt_1.expectedExclusion.setSource( path, type, object = None, index = None )
-T2tt_1.expectedExclusionM1.setSource( path, type, object = None, index = None )
-T2tt_1.expectedExclusionP1.setSource( path, type, object = None, index = None )
+T2tt_1.exclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'Obs_T2tt_MultiJet_Jet2b_MuJet', index = None )
+T2tt_1.exclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ObsMinus_T2tt_MultiJet_Jet2b_MuJet', index = None )
+T2tt_1.exclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ObsPlus_T2tt_MultiJet_Jet2b_MuJet', index = None )
+T2tt_1.expectedExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'Exp_T2tt_MultiJet_Jet2b_MuJet', index = None )
+T2tt_1.expectedExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ExpMinus_T2tt_MultiJet_Jet2b_MuJet', index = None )
+T2tt_1.expectedExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ExpPlus_T2tt_MultiJet_Jet2b_MuJet', index = None )
 #----global url settings ----
-T2tt_1.dataUrl =
-T2tt_1.histoDataUrl =
+T2tt_1.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/xsecUL_SMS_Razor.root'
+#T2tt_1.histoDataUrl =
 #----limit url settings ----
-T2tt_1.limit.dataUrl =
-T2tt_1.expectedlimit.dataUrl =
+#T2tt_1.limit.dataUrl =
+#T2tt_1.expectedlimit.dataUrl =
 #----exclusion url settings ----
-T2tt_1.exclusionDataUrl =
-T2tt_1.exclusion.dataUrl =
-T2tt_1.exclusionM1.dataUrl =
-T2tt_1.exclusionP1.dataUrl =
-T2tt_1.expectedExclusion.dataUrl =
-T2tt_1.expectedExclusionM1.dataUrl =
-T2tt_1.expectedExclusionP1.dataUrl =
+#T2tt_1.exclusionDataUrl =
+#T2tt_1.exclusion.dataUrl =
+#T2tt_1.exclusionM1.dataUrl =
+#T2tt_1.exclusionP1.dataUrl =
+#T2tt_1.expectedExclusion.dataUrl =
+#T2tt_1.expectedExclusionM1.dataUrl =
+#T2tt_1.expectedExclusionP1.dataUrl =
 
 #+++++++ next txName block ++++++++++++++
 T1tttt = TxName('T1tttt')
-T1tttt.on.checked =
-T1tttt.off.checked =
-T1tttt.on.constraint =
-T1tttt.off.constraint =
-T1tttt.on.condition =
-T1tttt.off.condition =
-T1tttt.on.fuzzycondition =
-T1tttt.off.fuzzycondition =
+#T1tttt.on.checked = 
+#T1tttt.off.checked =
+T1tttt.on.constraint = "[[['t','t']],[['t','t']]]"
+T1tttt.off.constraint = "[[['b','W','b','W']],[['b','W','b','W']]]"
+T1tttt.on.condition = None
+T1tttt.off.condition = None
+T1tttt.on.fuzzycondition = None
+T1tttt.off.fuzzycondition = None
 
 #+++++++ next mass plane block ++++++++++++++
-T1tttt_1 = T1tttt.setMassPlane(motherMass = , lspMass = )
+T1tttt_1 = T1tttt.addMassPlane(motherMass = x , lspMass = y)
 #----figure----
-T1tttt_1.figure =
-T1tttt_1.figureUrl =
+T1tttt_1.figure = 'Figure14'
+T1tttt_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T2ttRazorXSEC.pdf'
 #----limit source----
-T1tttt_1.limit.setSource( path, type, object = None, index = None )
-T1tttt_1.expectedlimit.setSource( path, type, object = None, index = None )
+T1tttt_1.limit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Obs_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.expectedlimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'xsecUL_Exp_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
 #----exclusion source----
-T1tttt_1.exclusion.setSource( path, type, object = None, index = None )
-T1tttt_1.exclusionM1.setSource( path, type, object = None, index = None )
-T1tttt_1.exclusionP1.setSource( path, type, object = None, index = None )
-T1tttt_1.expectedExclusion.setSource( path, type, object = None, index = None )
-T1tttt_1.expectedExclusionM1.setSource( path, type, object = None, index = None )
-T1tttt_1.expectedExclusionP1.setSource( path, type, object = None, index = None )
+T1tttt_1.exclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName= 'Obs_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.exclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ObsMinus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.exclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ObsPlus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.expectedExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'Exp_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.expectedExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ExpMinus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.expectedExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ExpPlus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
 #----global url settings ----
-T1tttt_1.dataUrl =
-T1tttt_1.histoDataUrl =
+T1tttt_1.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/xsecUL_SMS_Razor.root'
+#T1tttt_1.histoDataUrl =
 #----limit url settings ----
-T1tttt_1.limit.dataUrl =
-T1tttt_1.expectedlimit.dataUrl =
+#T1tttt_1.limit.dataUrl =
+#T1tttt_1.expectedlimit.dataUrl =
 #----exclusion url settings ----
-T1tttt_1.exclusionDataUrl =
-T1tttt_1.exclusion.dataUrl =
-T1tttt_1.exclusionM1.dataUrl =
-T1tttt_1.exclusionP1.dataUrl =
-T1tttt_1.expectedExclusion.dataUrl =
-T1tttt_1.expectedExclusionM1.dataUrl =
-T1tttt_1.expectedExclusionP1.dataUrl ='''
+#T1tttt_1.exclusionDataUrl =
+#T1tttt_1.exclusion.dataUrl =
+#T1tttt_1.exclusionM1.dataUrl =
+#T1tttt_1.exclusionP1.dataUrl =
+#T1tttt_1.expectedExclusion.dataUrl =
+#T1tttt_1.expectedExclusionM1.dataUrl =
+#T1tttt_1.expectedExclusionP1.dataUrl =
 
 databaseCreator.create()
