6c6dabc74a60da033c1afb4adcd609ab17b19038
walten <wolfgang.waltenberger@gmail.com>
2015-03-23 16:42:10 -0300

    validation added

---
+++ 8TeV/ATLAS/ATLAS-CONF-2013-053-eff/validation/plotValidation.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import argparse, types
+argparser = argparse.ArgumentParser(description="validates one pair of txname / axes")
+argparser.add_argument ( '-T', '--txname', nargs='?', help='txname [default T1]',
+                type=types.StringType, default='T1' )
+argparser.add_argument ( '-p', '--plottype', nargs='?', help=
+                'plottype [plain, bestcut, upperlimits, crosssections. default plain]',
+                type=types.StringType, default='plain' )
+argparser.add_argument ( '-a', '--axes', nargs='?', help='axes description [default 2*Eq(mother,x)_Eq(lsp,y)]',
+                type=types.StringType, default='2*Eq(mother,x)_Eq(lsp,y)' )
+args=argparser.parse_args() 
+
+
+import sys,os
+sys.path.insert(0,"../../../../../smodels-utils/")
+sys.path.insert(0,"../../../../../smodels/")
+
+from smodels.experiment.databaseObjects import DataBase
+from validation.plotProducer import ValidationPlot, getExpIdFromPath
+from smodels.tools.physicsUnits import pb, fb
+NAN=float('nan')
+
+## ValidationPlot.computeWrongnessFactor = computeWrongnessFactor
+
+filename="%s_%s.py" % ( args.txname, args.axes.replace("(","").replace(")","").replace(",","").replace("*","") )
+execfile(filename)
+
+database = DataBase("../../../../")
+expRes = database.getExpResults(analysisIDs=[ getExpIdFromPath() ],datasetIDs=[None])
+
+
+plot=ValidationPlot( expRes, args.txname, args.axes )
+plot.data=validationData
+agreement = plot.computeAgreementFactor()
+print "agreement=",agreement
+
+if args.plottype=="plain":
+    plot.getPlot()
+else:
+    plot.getSpecialPlot( what=args.plottype )
+plot.savePlot()
+
+# import IPython
+# IPython.embed()
