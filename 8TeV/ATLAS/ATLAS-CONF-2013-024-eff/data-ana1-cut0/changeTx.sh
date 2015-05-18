baad5509d848b4636951d7d09222e9090fd3ff6e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2015-05-18 18:44:07 -0300

    trashing ana

--- 8TeV/ATLAS/ATLAS-CONF-2013-024-eff/data-ana1-cut0/changeTx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for FILE in `ls T*.txt`; do
-	echo $FILE
-	cat $FILE | sed -e 's/txname/txName/' | sed -e 's/^condition/conditionDescription/' | sed -e 's/fuzzycondition/condition/' > ${FILE}.new
-done
