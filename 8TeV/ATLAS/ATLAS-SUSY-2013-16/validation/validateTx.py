11204199be77153a2b5480a00ff2f40a97448796
fambrogi <fambrogi@cern.ch>
2016-05-13 10:22:24 +0200

    Updated ATLAS-SUSY-2013-16

--- 8TeV/ATLAS/ATLAS-SUSY-2013-16/validation/validateTx.py
+++ 8TeV/ATLAS/ATLAS-SUSY-2013-16/validation/validateTx.py
@@ -18,13 +18,13 @@ sys.path.insert(0,os.path.join(home,"smodels-utils"))
 sys.path.insert(0,os.path.join(home,"smodels/"))
 
 from validation.plotProducer import validateTxName,validatePlot,validateExpRes, getExpIdFromPath, getDatasetIdsFromPath
-from smodels.experiment.databaseObj import Database
+from smodels.experiment.databaseObjects import Database
 import logging
 from smodels.theory.crossSection import logger as cl
 from smodels.theory.slhaDecomposer import logger as dl
-from smodels.experiment.txnameObj import logger as tl
-cl.setLevel(level=logging.INFO)
-dl.setLevel(level=logging.INFO)
+from smodels.experiment.txnameObject import logger as tl
+cl.setLevel(level=logging.DEBUG)
+dl.setLevel(level=logging.DEBUG)
 tl.setLevel(level=logging.INFO)
 
 database = Database(os.path.join(home,"smodels-database"))
