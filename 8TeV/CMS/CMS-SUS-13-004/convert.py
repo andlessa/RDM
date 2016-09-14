01289defe012d61051926a1d44f150a7fd11a063
fambrogi <fambrogi@cern.ch>
2016-09-14 17:39:56 +0200

    Re-implemented CMS-SUS-13-004 since oridingal twiki and data changed

--- 8TeV/CMS/CMS-SUS-13-004/convert.py
+++ 8TeV/CMS/CMS-SUS-13-004/convert.py
@@ -2,11 +2,9 @@
 
 """
 .. module:: convert
-   :synopsis: uesed to create info.txt, txname.txt, twiki.txt and sms.py.
+   :synopsis: used to create info.txt and the <txname>.txt files.
 
-.. moduleauthor:: Michael Traub <michael.traub@gmx.at>
-
-"""   
+"""
 import sys
 import os
 import argparse
@@ -14,17 +12,17 @@ import types
 
 argparser = argparse.ArgumentParser(description = \
 'create info.txt, txname.txt, twiki.txt and sms.py')
-argparser.add_argument ('-utilsPath', '--utilsPath', \
+argparser.add_argument ('-utilsPath', '--utilsPath', 
 help = 'path to the package smodels_utils',\
 type = types.StringType)
 args = argparser.parse_args()
-    
+
 if args.utilsPath:
     utilsPath = args.utilsPath
 else:
     databaseRoot = '../../../'
     sys.path.append(os.path.abspath(databaseRoot))
-    from utilsPath import utilsPath 
+    from utilsPath import utilsPath
     utilsPath = databaseRoot + utilsPath
 
 sys.path.append(os.path.abspath(utilsPath))
@@ -38,136 +36,138 @@ info.url = 'https://twiki.cern.ch/twiki/bin/view/CMSPublic/PhysicsResultsSUS1300
 info.sqrts = 8
 info.lumi = 19.3
 info.prettyName = 'Razor with b-jets'
-info.private  = False
-#info.arxiv =
+info.private = False
+info.arxiv = 'http://arxiv.org/abs/1502.00300v2'
 #info.contact =
-#info.publication =
-info.comment = 'Not updated with arXiv publication results, supersededBy to be set after arXiv results?'
+info.publication = 'http://dx.doi.org/10.1103/PhysRevD.91.052018'
+info.comment ='Other topologies have a compressed mass spectrum (decays with intermediate charginos, <= 5 GeV mass difference)'
 #info.supersedes =
-#info.supersededBy =
-info.implementedBy = 'Suchita Kulkarni'
+#info.supersededBy = 'CMS-SUS-PAS-14-011'
+
+#+++++++ next txName block ++++++++++++++
+T2tt = TxNameInput('T2tt')
+T2tt.on.checked =''
+T2tt.off.checked =''
+T2tt.on.constraint = "[[['t']],[['t']]]"
+T2tt.off.constraint =  "[[['b','W']],[['b','W']]]"
+T2tt.on.conditionDescription = None
+T2tt.off.conditionDescription = None
+T2tt.on.condition = None
+T2tt.off.condition = None
+#T2tt.branchingRatio =
+
+#+++++++ next mass plane block ++++++++++++++
+T2tt_1 = T2tt.addMassPlane(motherMass =x , lspMass =y)
+#----figure----
+T2tt_1.figure = 'Fig. 15c'
+T2tt_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T2ttHybridNew0Lp1Lp2LXSEC.png'
+#----limit source----
+T2tt_1.obsUpperLimit.setSource( 'orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 2 )
+#T2tt_1.expUpperLimit.setSource( 'orig/T2tt_Comb.root', 'root', objectName = 'cCONT_', index = None)
+#----exclusion source----
+T2tt_1.obsExclusion.setSource(  'orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 8 )
+T2tt_1.obsExclusionM1.setSource('orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 10 )
+T2tt_1.obsExclusionP1.setSource('orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 9 )
+T2tt_1.expExclusion.setSource(  'orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 5 )
+T2tt_1.expExclusionM1.setSource('orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 7 )
+T2tt_1.expExclusionP1.setSource('orig/T2tt_Comb.root', 'canvas', objectName = 'cCONT_', index = 6 )
+#----global url settings ----
+T2tt_1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+#----limit url settings ----
+T2tt_1.histoDataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.obsUpperLimit.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+#T2tt_1.expUpperlimit.dataUrl =
+#----exclusion url settings ----
+T2tt_1.exclusionDataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.obsExclusion.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.obsExclusionM1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.obsExclusionP1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.expExclusion.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.expExclusionM1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T2tt_1.expExclusionP1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
 
 #+++++++ next txName block ++++++++++++++
 T1bbbb = TxNameInput('T1bbbb')
-#T1bbbb.on.checked =
-#T1bbbb.off.checked =
+T1bbbb.on.checked =''
+T1bbbb.off.checked =''
 T1bbbb.on.constraint = "[[['b','b']],[['b','b']]]"
 #T1bbbb.off.constraint =
 T1bbbb.on.conditionDescription = None
 #T1bbbb.off.conditionDescription =
 T1bbbb.on.condition = None
-T1bbbb.off.condition = None
+#T1bbbb.off.condition =
+#T1bbbb.branchingRatio =
 
 #+++++++ next mass plane block ++++++++++++++
-T1bbbb_1 = T1bbbb.addMassPlane(motherMass = x, lspMass = y)
+T1bbbb_1 = T1bbbb.addMassPlane(motherMass =x , lspMass =y )
 #----figure----
-T1bbbb_1.figure = 'Figure 12'
-T1bbbb_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T1bbbbRazorXSEC.pdf'
+T1bbbb_1.figure = 'Fig. 13a'
+T1bbbb_1.figureUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T1bbbbHybridNew0LXSEC.png'
 #----limit source----
-T1bbbb_1.obsUpperLimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = "xsecUL_Obs_T1bbbb_MultiJet_Jet2b")
-T1bbbb_1.expUpperLimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Exp_T1bbbb_MultiJet_Jet2b', index = None )
+T1bbbb_1.obsUpperLimit.setSource( 'orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 2  )
+#T1bbbb_1.expUpperLimit.setSource( 'orig/T1bbbb.root', 'root', objectName = 'cCONT_', index = None   )
 #----exclusion source----
-T1bbbb_1.obsExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'Obs_T1bbbb_MultiJet_Jet2b', index = None )
-T1bbbb_1.obsExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ObsMinus_T1bbbb_MultiJet_Jet2b' , index = None )
-T1bbbb_1.obsExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ObsPlus_T1bbbb_MultiJet_Jet2b', index = None )
-T1bbbb_1.expExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'Exp_T1bbbb_MultiJet_Jet2b', index = None )
-T1bbbb_1.expExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ExpMinus_T1bbbb_MultiJet_Jet2b', index = None )
-T1bbbb_1.expExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ExpPlus_T1bbbb_MultiJet_Jet2b', index = None )
+T1bbbb_1.obsExclusion.setSource(  'orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 8  )
+T1bbbb_1.obsExclusionM1.setSource('orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 10   )
+T1bbbb_1.obsExclusionP1.setSource('orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 9   )
+T1bbbb_1.expExclusion.setSource(  'orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 5   )
+T1bbbb_1.expExclusionM1.setSource('orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 7   )
+T1bbbb_1.expExclusionP1.setSource('orig/T1bbbb.root', 'canvas', objectName = 'cCONT_', index = 6   )
 #----global url settings ----
-T1bbbb_1.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/xsecUL_SMS_Razor.root'
+T1bbbb_1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
 #----limit url settings ----
-#T1bbbb_1.obsUpperLimit.dataUrl =
-#T1bbbb_1.expUpperLimit.dataUrl =
-#T1bbbb_1.histoDataUrl =
+T1bbbb_1.histoDataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.obsUpperLimit.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+#T1bbbb_1.expUpperlimit.dataUrl =
 #----exclusion url settings ----
-#T1bbbb_1.exclusionDataUrl =
-#T1bbbb_1.obsExclusion.dataUrl =
-#T1bbbb_1.obsExclusionM1.dataUrl =
-#T1bbbb_1.obsExclusionP1.dataUrl =
-#T1bbbb_1.expExclusion.dataUrl =
-#T1bbbb_1.expExclusionM1.dataUrl =
-#T1bbbb_1.expExclusionP1.dataUrl =
-
-#+++++++ next txName block ++++++++++++++
-T2tt = TxNameInput('T2tt')
-#T2tt.on.checked =
-#T2tt.off.checked =
-T2tt.on.constraint = "[[['t']],[['t']]]"
-T2tt.off.constraint = "[[['b','W']],[['b','W']]]"
-T2tt.on.conditionDescription = None
-T2tt.off.conditionDescription = None
-T2tt.on.condition = None
-T2tt.off.condition = None
-
-#+++++++ next mass plane block ++++++++++++++
-T2tt_1 = T2tt.addMassPlane(motherMass = x, lspMass = y )
-#----figure----
-T2tt_1.figure = 'Figure 13'
-T2tt_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T1ttttRazorXSEC.pdf'
-#----limit source----
-T2tt_1.obsUpperLimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Obs_T2tt_MultiJet_Jet2b_MuJet', index = None )
-T2tt_1.expUpperLimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Exp_T2tt_MultiJet_Jet2b_MuJet', index = None )
-#----exclusion source----
-T2tt_1.obsExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'Obs_T2tt_MultiJet_Jet2b_MuJet', index = None )
-T2tt_1.obsExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ObsMinus_T2tt_MultiJet_Jet2b_MuJet', index = None )
-T2tt_1.obsExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ObsPlus_T2tt_MultiJet_Jet2b_MuJet', index = None )
-T2tt_1.expExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'Exp_T2tt_MultiJet_Jet2b_MuJet', index = None )
-T2tt_1.expExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ExpMinus_T2tt_MultiJet_Jet2b_MuJet', index = None )
-T2tt_1.expExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'ExpPlus_T2tt_MultiJet_Jet2b_MuJet', index = None )
-#----global url settings ----
-T2tt_1.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/xsecUL_SMS_Razor.root'
-#T2tt_1.histoDataUrl =
-#----limit url settings ----
-#T2tt_1.obsUpperLimit.dataUrl =
-#T2tt_1.expUpperLimit.dataUrl =
-#----exclusion url settings ----
-#T2tt_1.exclusionDataUrl =
-#T2tt_1.obsExclusion.dataUrl =
-#T2tt_1.obsExclusionM1.dataUrl =
-#T2tt_1.obsExclusionP1.dataUrl =
-#T2tt_1.expExclusion.dataUrl =
-#T2tt_1.expExclusionM1.dataUrl =
-#T2tt_1.expExclusionP1.dataUrl =
+T1bbbb_1.exclusionDataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.obsExclusion.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.obsExclusionM1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.obsExclusionP1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.expExclusion.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.expExclusionM1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1bbbb_1.expExclusionP1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
 
 #+++++++ next txName block ++++++++++++++
 T1tttt = TxNameInput('T1tttt')
-#T1tttt.on.checked = 
-#T1tttt.off.checked =
+T1tttt.on.checked =''
+T1tttt.off.checked =''
 T1tttt.on.constraint = "[[['t','t']],[['t','t']]]"
 T1tttt.off.constraint = "[[['b','W','b','W']],[['b','W','b','W']]]"
 T1tttt.on.conditionDescription = None
-T1tttt.off.conditionDescription = None
+T1tttt.off.conditionDescription =None
 T1tttt.on.condition = None
-T1tttt.off.condition = None
+T1tttt.off.condition =None
+#T1tttt.branchingRatio =
 
 #+++++++ next mass plane block ++++++++++++++
-T1tttt_1 = T1tttt.addMassPlane(motherMass = x , lspMass = y)
+T1tttt_1 = T1tttt.addMassPlane(motherMass =x, lspMass =y )
 #----figure----
-T1tttt_1.figure = 'Figure14'
-T1tttt_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T2ttRazorXSEC.pdf'
+T1tttt_1.figure = 'Fig. 13e'
+T1tttt_1.figureUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/T1ttttHybridNew0Lp1Lp2LXSEC.pdf'
 #----limit source----
-T1tttt_1.obsUpperLimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root', objectName = 'xsecUL_Obs_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
-T1tttt_1.expUpperLimit.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'xsecUL_Exp_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.obsUpperLimit.setSource(  'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 2  )
+#T1tttt_1.expUpperLimit.setSource(  'orig/T1tttt.root', 'root', objectName = 'cCONT_', index = None  )
 #----exclusion source----
-T1tttt_1.obsExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName= 'Obs_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
-T1tttt_1.obsExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ObsMinus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
-T1tttt_1.obsExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ObsPlus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
-T1tttt_1.expExclusion.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'Exp_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
-T1tttt_1.expExclusionM1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ExpMinus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
-T1tttt_1.expExclusionP1.setSource( './orig/xsecUL_SMS_Razor.root', 'root',  objectName = 'ExpPlus_T1tttt_MultiJet_EleMultiJet_MuMultiJet', index = None )
+T1tttt_1.obsExclusion.setSource(   'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 8  )
+T1tttt_1.obsExclusionM1.setSource( 'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 10   )
+T1tttt_1.obsExclusionP1.setSource( 'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 9  )
+T1tttt_1.expExclusion.setSource(   'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 5  )
+T1tttt_1.expExclusionM1.setSource( 'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 7  )
+T1tttt_1.expExclusionP1.setSource( 'orig/T1tttt.root', 'canvas', objectName = 'cCONT_', index = 6  )
 #----global url settings ----
-T1tttt_1.dataUrl = 'https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/xsecUL_SMS_Razor.root'
-#T1tttt_1.histoDataUrl =
+T1tttt_1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
 #----limit url settings ----
-#T1tttt_1.obsUpperLimit.dataUrl =
-#T1tttt_1.expUpperLimit.dataUrl =
+T1tttt_1.histoDataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.obsUpperLimit.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+#T1tttt_1.expUpperlimit.dataUrl =
 #----exclusion url settings ----
-#T1tttt_1.exclusionDataUrl =
-#T1tttt_1.obsExclusion.dataUrl =
-#T1tttt_1.obsExclusionM1.dataUrl =
-#T1tttt_1.obsExclusionP1.dataUrl =
-#T1tttt_1.expExclusion.dataUrl =
-#T1tttt_1.expExclusionM1.dataUrl =
-#T1tttt_1.expExclusionP1.dataUrl =
+T1tttt_1.exclusionDataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.obsExclusion.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.obsExclusionM1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.obsExclusionP1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.expExclusion.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.expExclusionM1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
+T1tttt_1.expExclusionP1.dataUrl ='https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13004/razor_8TeV_sms_results.root'
 
 databaseCreator.create()
--- 8TeV/CMS/CMS-SUS-13-004/validation/validate.py
+++ 8TeV/CMS/CMS-SUS-13-004/validation/validate.py
@@ -2,6 +2,8 @@
 
 import sys,os
 home = os.path.expanduser("~")
+home = "../../../../../"
+
 sys.path.insert(0,os.path.join(home,"smodels-utils/"))
 sys.path.insert(0,os.path.join(home,"smodels/"))
 
@@ -24,10 +26,25 @@ print "exp id=",getExpIdFromPath(),"datasetid=",getDatasetIdsFromPath()
 database = Database(os.path.join(home,"smodels-database"))
 #How to validate all plots for all Txnames in one ExpRes:
 expRes = database.getExpResults(analysisIDs=[getExpIdFromPath()],datasetIDs=getDatasetIdsFromPath() )
+if expRes == []:
+    print "[validate.py] Error: could not find any experimental results."
+    f=open("../globalInfo.txt")
+    lines=f.readlines()
+    f.close()
+    for line in lines:
+        if line[:3]=="id:":
+            Id=line[4:]
+            Id=Id.replace("\n","")
+            if Id != getExpIdFromPath():
+                print "[validate.py] Error: path directory ``%s'' does not match exp id ``%s'' in ../globalInfo.txt" % ( getExpIdFromPath(), Id  )
+                sys.exit()
+
 slhamain = os.path.join(home,"smodels-utils/slha")
-kfactorDict = { "TChiWZ": 1.2, "TChiWW": 1.2, "TChiChipmSlepL": 1.2, "TChiChipmSlepStau": 1.2, "TChiChipmStauStau": 1.2, \
-                "TChiSlepSnu": 1.2, "TChiStauSnu": 1.2, "TChiWH": 1.2, "TChiWZoff": 1.2 }
-## kfactorDict= {} 
+kfactorDict = { "TChiWZ": 1.2, "TChiWW": 1.2, "TChiChipmSlepL": 1.2, 
+                "TChiChipmSlepStau": 1.2, "TChiChipmStauStau": 1.2, 
+                "TChiSlepSnu": 1.2, "TChiStauSnu": 1.2, "TChiWH": 1.2, 
+                "TChiWZoff": 1.2 }
+#kfactorDict= {} 
 for i in expRes:
     validateExpRes(i,slhamain, kfactorDict = kfactorDict )
 
