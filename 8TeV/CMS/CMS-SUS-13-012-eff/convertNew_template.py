c1b2f8fbf28b6acf933fffaae0bc895edf6793fd
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-05-23 11:19:45 +0200

    updated

--- 8TeV/CMS/CMS-SUS-13-012-eff/convertNew_template.py
+++ 8TeV/CMS/CMS-SUS-13-012-eff/convertNew_template.py
@@ -34,6 +34,7 @@ from smodels_utils.dataPreparation.inputObjects import MetaInfoInput,DataSetInpu
 from smodels_utils.dataPreparation.databaseCreation import databaseCreator
 from smodels_utils.dataPreparation.massPlaneObjects import x, y, z
 
+databaseCreator.ncpus = 2
 DataSetInput.ntoys = 10
 
 #+++++++ global info block ++++++++++++++
@@ -108,6 +109,7 @@ T2bb.conditionDescription ="None"
 T2bb.condition ="None"
 T2bb.massConstraint = None
 T2bb.source = 'SModelS'
+T2bb.dataUrl = None
 
 T2tt = dataset.addTxName('T2tt')
 T2tt.constraint ="[[['t']],[['t']]]"
@@ -120,42 +122,52 @@ T2ttoff = dataset.addTxName('T2ttoff')
 T2ttoff.constraint ="[[['W','b']],[['W','b']]]"
 T2ttoff.conditionDescription ="None"
 T2ttoff.condition ="None"
-T2ttoff.massConstraint = None
+T2ttoff.massConstraint = [[ 'dm <= 169.' ]]*2
 T2ttoff.source = 'SModelS'
+T2ttoff.dataUrl = None
 
-T1tttt = T1tttt.addMassPlane([[x,y]]*2)
-T1tttt.figure = "Fig_7c"
-T1tttt.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
-T1tttt.dataUrl = None
-T1tttt.addSource('obsExclusion', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclOneTimesProspino")
-T1tttt.addSource('obsExclusionM1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclMinusSysErrProspino")
-T1tttt.addSource('obsExclusionP1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclPlusSysErrProspino")
-T1tttt.addSource('expExclusion', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclOneTimesProspino")
-T1tttt.addSource('expExclusionM1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclMinusOneSigmaProspino")
-T1tttt.addSource('expExclusionP1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclPlusOneSigmaProspino")
-T1tttt.addSource('efficiencyMap',"orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName ="h_EffAcc_$datasetStr$")
+T1tttt_1 = T1tttt.addMassPlane([[x,y]]*2)
+T1tttt_1.figure = "Fig_7c"
+T1tttt_1.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
+T1tttt_1.dataUrl = None
+T1tttt_1.addSource('obsExclusion', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclOneTimesProspino")
+T1tttt_1.addSource('obsExclusionM1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclMinusSysErrProspino")
+T1tttt_1.addSource('obsExclusionP1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclPlusSysErrProspino")
+T1tttt_1.addSource('expExclusion', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclOneTimesProspino")
+T1tttt_1.addSource('expExclusionM1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclMinusOneSigmaProspino")
+T1tttt_1.addSource('expExclusionP1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclPlusOneSigmaProspino")
+T1tttt_1.addSource('efficiencyMap',"orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName ="h_EffAcc_$datasetStr$")
 
-T1bbbb = T1bbbb.addMassPlane([[x,y]]*2)
-T1bbbb.figure = "Fig_7c"
-T1bbbb.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
-T1bbbb.dataUrl = None
-T1bbbb.addSource('efficiencyMap',"orig/cms_sus_13_012_T1bbbb_1_EM_MAPS/MA5_EM_T1bbbb_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
+T1ttttoff_1 = T1ttttoff.addMassPlane([[x,y]]*2)
+T1ttttoff_1.figure = "Fig_7c"
+T1ttttoff_1.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
+T1ttttoff_1.dataUrl = None
+T1ttttoff_1.addSource('obsExclusion', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclOneTimesProspino")
+T1ttttoff_1.addSource('obsExclusionM1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclMinusSysErrProspino")
+T1ttttoff_1.addSource('obsExclusionP1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_obsExclPlusSysErrProspino")
+T1ttttoff_1.addSource('expExclusion', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclOneTimesProspino")
+T1ttttoff_1.addSource('expExclusionM1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclMinusOneSigmaProspino")
+T1ttttoff_1.addSource('expExclusionP1', "orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName = "combined_expExclPlusOneSigmaProspino")
+T1ttttoff_1.addSource('efficiencyMap',"orig/SUS13012_XsecLimits_T1tttt.root", "root", objectName ="h_EffAcc_$datasetStr$")
 
-T1btbt = T1btbt.addMassPlane([[x,y]]*2)
-T1btbt.figure = "Fig_7c"
-T1btbt.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
-T1btbt.dataUrl = None
-T1btbt.addSource('efficiencyMap',"orig/cms_sus_13_012_T1btbt_1_EM_MAPS/MA5_EM_T1btbt_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
+T1bbbb_1 = T1bbbb.addMassPlane([[x,y]]*2)
+T1bbbb_1.figure = "Fig_7c"
+T1bbbb_1.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
+T1bbbb_1.dataUrl = None
+T1bbbb_1.addSource('efficiencyMap',"orig/cms_sus_13_012_T1bbbb_1_EM_MAPS/MA5_EM_T1bbbb_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T1btbt_1 = T1btbt.addMassPlane([[x,y]]*2)
+T1btbt_1.figure = "Fig_7c"
+T1btbt_1.figureUrl = "https://twiki.cern.ch/twiki/pub/CMSPublic/PhysicsResultsSUS13012/Fig_7c.pdf"
+T1btbt_1.dataUrl = None
+T1btbt_1.addSource('efficiencyMap',"orig/cms_sus_13_012_T1btbt_1_EM_MAPS/MA5_EM_T1btbt_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
 
 T2bb_1 = T2bb.addMassPlane([[x,y]]*2)
-T2bb_1.figure = "FIXME"
-T2bb_1.figureUrl = "FIXME"
-T2bb_1.dataUrl = None
 T2bb_1.addSource('efficiencyMap',"orig/cms_sus_13_012_T2bb_1_EM_MAPS/MA5_EM_T2bb_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
 
 T2tt_1 = T2tt.addMassPlane([[x,y]]*2)
-T2tt_1.figure = "FIXME"
-T2tt_1.figureUrl = "FIXME"
+T2tt_1.figure = None
+T2tt_1.figureUrl = None
 T2tt_1.dataUrl = None
 T2tt_1.addSource('efficiencyMap',"orig/cms_sus_13_012_T2tt_1_EM_MAPS/MA5_EM_T2tt_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
 
@@ -165,7 +177,6 @@ T2ttoff_1.figureUrl = "FIXME"
 T2ttoff_1.dataUrl = None
 T2ttoff_1.addSource('efficiencyMap',"orig/cms_sus_13_012_T2tt_1_EM_MAPS/MA5_EM_T2tt_1_$datasetStr$.dat", "txt", objectName ="None", index = None )
 
-T1ttttoff.addMassPlane(T1tttt)
 #+++++++ next txName block ++++++++++++++
 T1 = dataset.addTxName('T1')
 T1.constraint ="[[['jet','jet']],[['jet','jet']]]"
@@ -193,14 +204,6 @@ T5ZZ.condition ="None"
 T5ZZ.massConstraint = None
 T5ZZ.source = 'SModelS'
 #+++++++ next txName block ++++++++++++++
-"""
-T5ZZoff = dataset.addTxName('T5ZZoff')
-T5ZZoff.constraint ="2.23*[[['jet','jet'],['jet','jet']],[['jet','jet'],['jet','jet']]]"
-T5ZZoff.conditionDescription ="None"
-T5ZZoff.condition =None
-T5ZZoff.massConstraint = [['dm >= 0.0','dm <= 86.']]*2
-T5ZZoff.source = 'SModelS'
-"""
 #+++++++ next mass plane block ++++++++++++++
 T5ZZ_x05 = T5ZZ.addMassPlane([[x,0.5*(x+y),y]]*2)
 T5ZZ_x05.dataUrl = None
@@ -215,6 +218,12 @@ T5ZZ_x095 = T5ZZ.addMassPlane([[x,0.95*x + 0.05*y,y]]*2)
 T5ZZ_x095.dataUrl = None
 T5ZZ_x095.addSource('efficiencyMap',"orig/T5ZZ_x095/MA5_EM_T5ZZ_Glu095Neu005_$datasetStr$.dat", "txt")
 """
+T5ZZoff = dataset.addTxName('T5ZZoff')
+T5ZZoff.constraint ="2.23*[[['jet','jet'],['jet','jet']],[['jet','jet'],['jet','jet']]]"
+T5ZZoff.conditionDescription ="None"
+T5ZZoff.condition =None
+T5ZZoff.massConstraint = [['dm >= 0.0','dm <= 86.']]*2
+T5ZZoff.source = 'SModelS'
 T5ZZoff.addMassPlane(T5ZZ_x05)
 T5ZZoff.addMassPlane(T5ZZ_x005)
 T5ZZoff.addMassPlane(T5ZZ_x095)
@@ -294,19 +303,14 @@ T5.conditionDescription ="None"
 T5.condition ="None"
 T5.massConstraint = None
 T5.source = 'SModelS'
+T5.dataUrl = None
 
 T5_x005 = T5.addMassPlane( [[x,0.05*x + 0.95*y,y]]*2 )
 T5_x005.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5_x005_EM_MAPS/MA5_EM_T5_x005_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5_x005.figureUrl = "FIXME" 
-T5_x005.dataUrl = "FIXME" 
 T5_x05 = T5.addMassPlane( [[x,0.5*x + 0.5*y,y]]*2 )
 T5_x05.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5_x05_EM_MAPS/MA5_EM_T5_x05_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5_x05.figureUrl = "FIXME" 
-T5_x05.dataUrl = "FIXME" 
 T5_x095 = T5.addMassPlane( [[x,0.95*x + 0.05*y,y]]*2 )
 T5_x095.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5_x005_EM_MAPS/MA5_EM_T5_x005_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5_x095.figureUrl = "FIXME" 
-T5_x095.dataUrl = "FIXME" 
 
 T5bbbb = dataset.addTxName('T5bbbb')
 T5bbbb.checked = ''
@@ -315,19 +319,14 @@ T5bbbb.conditionDescription ="None"
 T5bbbb.condition ="None"
 T5bbbb.massConstraint = None
 T5bbbb.source = 'SModelS'
+T5bbbb.dataUrl = None
 
 T5bbbb_x005 = T5bbbb.addMassPlane( [[x,0.05*x + 0.95*y,y]]*2 )
 T5bbbb_x005.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5bbbb_x005_EM_MAPS/MA5_EM_T5bbbb_x005_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5bbbb_x005.figureUrl = "FIXME" 
-T5bbbb_x005.dataUrl = "FIXME" 
 T5bbbb_x05 = T5bbbb.addMassPlane( [[x,0.5*x + 0.5*y,y]]*2 )
 T5bbbb_x05.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5bbbb_x05_EM_MAPS/MA5_EM_T5bbbb_x05_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5bbbb_x05.figureUrl = "FIXME" 
-T5bbbb_x05.dataUrl = "FIXME" 
 T5bbbb_x095 = T5bbbb.addMassPlane( [[x,0.95*x + 0.05*y,y]]*2 )
 T5bbbb_x095.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5bbbb_x005_EM_MAPS/MA5_EM_T5bbbb_x005_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5bbbb_x095.figureUrl = "FIXME" 
-T5bbbb_x095.dataUrl = "FIXME" 
 
 T5tttt = dataset.addTxName('T5tttt')
 T5tttt.checked = ''
@@ -336,36 +335,65 @@ T5tttt.conditionDescription ="None"
 T5tttt.condition ="None"
 T5tttt.massConstraint = None
 T5tttt.source = 'SModelS'
+T5tttt.dataUrl = None
 
 T5tttt_x05 = T5tttt.addMassPlane( [[x,0.5*x + 0.5*y,y]]*2 )
 T5tttt_x05.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5tttt_x05_EM_MAPS/MA5_EM_T5tttt_x05_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5tttt_x05.figureUrl = "FIXME" 
-T5tttt_x05.dataUrl = "FIXME" 
 
 T5tttt_p177 = T5tttt.addMassPlane( [[x, x-177.,y]]*2 )
 T5tttt_p177.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5tttt_DiffGluStop177_EM_MAPS/MA5_EM_T5tttt_DiffGluStop177_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5tttt_p177.figureUrl = "FIXME" 
-T5tttt_p177.dataUrl = "FIXME" 
 
 T5tttt_m177 = T5tttt.addMassPlane( [[x, y+177.,y]]*2 )
 T5tttt_m177.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T5tttt_DiffStopNeu177_EM_MAPS/MA5_EM_T5tttt_DiffStopNeu177_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T5tttt_m177.figureUrl = "FIXME" 
-T5tttt_m177.dataUrl = "FIXME" 
 
-"""
 T6bbWW = dataset.addTxName('T6bbWW')
 T6bbWW.checked = ''
-T6bbWW.constraint ="[[['t'],['t']],[['t'],['t']]]"
+T6bbWW.constraint = "[[['b'],['W']],[['b'],['W']]]"
 T6bbWW.conditionDescription ="None"
 T6bbWW.condition ="None"
 T6bbWW.massConstraint = None
 T6bbWW.source = 'SModelS'
+T6bbWW.dataUrl = None
 
 T6bbWW_x05 = T6bbWW.addMassPlane( [[x,0.5*x + 0.5*y,y]]*2 )
 T6bbWW_x05.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_x05_EM_MAPS/MA5_EM_T6bbWW_x05_$datasetStr$.dat", "txt", objectName ="None", index = None )
-T6bbWW_x05.figureUrl = "FIXME" 
-T6bbWW_x05.dataUrl = "FIXME" 
-"""
+
+T6bbWW_x01 = T6bbWW.addMassPlane( [[x,0.1 *x + 0.9 *y,y]]*2  )
+T6bbWW_x01.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_x01_EM_MAPS/MA5_EM_T6bbWW_x01_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWW_x09 = T6bbWW.addMassPlane( [[x,0.9 *x + 0.1 *y,y]]*2  )
+T6bbWW_x09.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_x09_EM_MAPS/MA5_EM_T6bbWW_x09_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWW_d10 = T6bbWW.addMassPlane( [[x, y+10. , y ]]*2  )
+T6bbWW_d10.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_DiffChargNeu10_EM_MAPS/MA5_EM_T6bbWW_DiffChargNeu10_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWW_d77 = T6bbWW.addMassPlane( [[x, y+77. , y ]]*2  )
+T6bbWW_d77.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_DiffChargNeu77_EM_MAPS/MA5_EM_T6bbWW_DiffChargNeu77_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWWoff = dataset.addTxName('T6bbWWoff')
+T6bbWWoff.checked = ''
+T6bbWWoff.constraint = "[[['b'],['jet','jet']],[['b'],['jet','jet']]]"
+T6bbWWoff.conditionDescription ="None"
+T6bbWWoff.condition ="None"
+T6bbWWoff.massConstraint = None
+T6bbWWoff.massConstraint = [['dm >= 0.0','dm <= 76.']]*2
+T6bbWWoff.source = 'SModelS'
+T6bbWWoff.dataUrl = None
+
+T6bbWWoff_x05 = T6bbWWoff.addMassPlane( [[x,0.5*x + 0.5*y,y]]*2 )
+T6bbWWoff_x05.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_x05_EM_MAPS/MA5_EM_T6bbWW_x05_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWWoff_x01 = T6bbWWoff.addMassPlane( [[x,0.1 *x + 0.9 *y,y]]*2  )
+T6bbWWoff_x01.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_x01_EM_MAPS/MA5_EM_T6bbWW_x01_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWWoff_x09 = T6bbWWoff.addMassPlane( [[x,0.9 *x + 0.1 *y,y]]*2  )
+T6bbWWoff_x09.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_x09_EM_MAPS/MA5_EM_T6bbWW_x09_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWWoff_d10 = T6bbWWoff.addMassPlane( [[x, y+10. , y ]]*2  )
+T6bbWWoff_d10.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_DiffChargNeu10_EM_MAPS/MA5_EM_T6bbWW_DiffChargNeu10_$datasetStr$.dat", "txt", objectName ="None", index = None )
+
+T6bbWWoff_d77 = T6bbWWoff.addMassPlane( [[x, y+77. , y ]]*2  )
+T6bbWWoff_d77.addSource ( "efficiencyMap", "orig/cms_sus_13_012_T6bbWW_DiffChargNeu77_EM_MAPS/MA5_EM_T6bbWW_DiffChargNeu77_$datasetStr$.dat", "txt", objectName ="None", index = None )
 
 END_BLOCK_TO_FILL
 
