fa4b60585cd8ed0370e0b1ec512b14025933a06a
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-06-27 16:06:32 +0200

    harmonize T2bbWWoff names

--- 8TeV/CMS/CMS-SUS-14-021-eff/convert.py
+++ 8TeV/CMS/CMS-SUS-14-021-eff/convert.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 .. module:: convert
@@ -61,7 +61,7 @@ T2bbWW.condition =None
 T2bbWW.massConstraint = None
 T2bbWW.source = 'CMS'
 #+++++++ next txName block ++++++++++++++
-T2bbWWoff = dataset.addTxName('T2bbWWoff')
+T2bbWWoff = dataset.addTxName('T2bbWWoffSemiLep')
 T2bbWWoff.constraint = "6.94*[[['b','mu','nu']],[['b','jet','jet']]]"
 T2bbWWoff.conditionDescription =None
 T2bbWWoff.condition =None
@@ -95,7 +95,7 @@ T2bbWW.condition =None
 T2bbWW.massConstraint = None
 T2bbWW.source = 'CMS'
 #+++++++ next txName block ++++++++++++++
-T2bbWWoff = dataset.addTxName('T2bbWWoff')
+T2bbWWoff = dataset.addTxName('T2bbWWoffSemiLep')
 T2bbWWoff.constraint = "6.94*[[['b','mu','nu']],[['b','jet','jet']]]"
 T2bbWWoff.conditionDescription =None
 T2bbWWoff.condition =None
@@ -129,7 +129,7 @@ T2bbWW.condition =None
 T2bbWW.massConstraint = None
 T2bbWW.source = 'CMS'
 #+++++++ next txName block ++++++++++++++
-T2bbWWoff = dataset.addTxName('T2bbWWoff')
+T2bbWWoff = dataset.addTxName('T2bbWWoffSemiLep')
 T2bbWWoff.constraint = "6.94*[[['b','mu','nu']],[['b','jet','jet']]]"
 T2bbWWoff.conditionDescription =None
 T2bbWWoff.condition =None
