522c124e99b8bf8a1ee101050a319ed6b1a36173
Andre Lessa <lessa.a.p@gmail.com>
2020-03-18 06:31:00 -0300

    Started CMS-SUS-16-032 implementation

---
+++ myCheckMateFiles/tools/analysis/include/analyses/CMS_13TeV/cms_sus_16_032.h
@@ -0,0 +1,19 @@
+#ifndef CMS_SUS_16_032_H_
+#define CMS_SUS_16_032_H_
+// AUTHOR: Andre Lessa
+//  EMAIL: andre.lessa@ufabc.edu.br
+#include "AnalysisBase.h"
+
+class Cms_sus_16_032 : public AnalysisBase {
+  public:
+    Cms_sus_16_032() : AnalysisBase()  {}               
+    ~Cms_sus_16_032() {}
+  
+    void initialize();
+    void analyze();        
+    void finalize();
+
+  private:
+};
+
+#endif
