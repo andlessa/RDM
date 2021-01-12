3a064fc63558e4b752cf44c6b2536ef157cd9413
Andre Lessa <lessa.a.p@gmail.com>
2021-01-12 12:51:48 -0300

    Updated CMS-SUS-19-009 (validated)

--- 13TeV/CMS/CMS-SUS-19-009/convert.py
+++ 13TeV/CMS/CMS-SUS-19-009/convert.py
@@ -9,33 +9,34 @@ import sys
 import os
 import argparse
 
-argparser = argparse.ArgumentParser(description =  
-'create info.txt, txname.txt, twiki.txt and sms.py')
-argparser.add_argument ('-utilsPath', '--utilsPath', 
-help = 'path to the package smodels_utils',\
-type = str )
-argparser.add_argument ('-smodelsPath', '--smodelsPath', 
-help = 'path to the package smodels_utils',\
-type = str )
+argparser = argparse.ArgumentParser(description =
+    'create info.txt, txname.txt, twiki.txt and sms.py')
+argparser.add_argument ('-utilsPath', '--utilsPath',
+    help = 'path to the package smodels_utils',\
+    type = str, default = '~/smodels-utils' )
+argparser.add_argument ('-smodelsPath', '--smodelsPath',
+    help = 'path to the package smodels_utils',\
+    type = str, default = '~/smodels' )
 argparser.add_argument ('-no', '--noUpdate',
-help = 'do not update the lastUpdate field.',\
-action= "store_true" )
+    help = 'do not update the lastUpdate field.',\
+    action= "store_true" )
 argparser.add_argument ('-r', '--resetValidation',
-help = 'reset the validation flag',\
-action= "store_true" )
+    help = 'reset the validation flag',\
+    action= "store_true" )
+
 args = argparser.parse_args()
 
-if args.utilsPath:
-    utilsPath = args.utilsPath
-else:
-    databaseRoot = '../../../'
-    sys.path.append(os.path.abspath(databaseRoot))
-    from utilsPath import utilsPath
-    utilsPath = databaseRoot + utilsPath
-if args.smodelsPath:
-    sys.path.append(os.path.abspath(args.smodelsPath))
+if args.noUpdate:
+    os.environ["SMODELS_NOUPDATE"]="1"
 
-sys.path.append(os.path.abspath(utilsPath))
+if args.resetValidation:
+    os.environ["SMODELS_RESETVALIDATION"]="1"
+
+utilsPath = args.utilsPath
+sys.path.append(os.path.abspath(os.path.expanduser(utilsPath)))
+sys.path.append(os.path.abspath(os.path.expanduser(args.smodelsPath)))
+
+
 from smodels_utils.dataPreparation.inputObjects import MetaInfoInput,DataSetInput
 from smodels_utils.dataPreparation.databaseCreation import databaseCreator
 from smodels_utils.dataPreparation.massPlaneObjects import x, y, z
@@ -57,10 +58,9 @@ info.comment     = 'First attempt at PDF-based extraction'
 dataset = DataSetInput('data')
 dataset.setInfo(dataType = 'upperLimit', dataId = None)
 
-#====================   T2tt   ==============================  
+#====================   T2tt   ==============================
 
 #+++++txName block +++++++++++++++++
-
 T2tt=dataset.addTxName('T2tt')
 T2tt.checked=''
 T2tt.constraint="[[['t']],[['t']]]"
@@ -68,22 +68,22 @@ T2tt.condition=None
 T2tt.conditionDescription = None
 T2tt.massConstraint=[['dm>169'],['dm>169']]
 T2tt.source="CMS"
-
 #++++++next mass plane block+++++++++
-
 T2tt_1 = T2tt.addMassPlane(2*[[x,y]])
 T2tt_1.figure='Fig. 6'
 T2tt_1.figureUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_006.png'
 T2tt_1.dataUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_006.pdf'
 T2tt_1.exclusionDataUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_006.pdf'
 
-T2tt_1.setSources( dataLabels=['upperLimits'],
-                   dataFiles=['orig/CMS-SUS-19-009_Figure_006.pdf'], 
-                   indices=['x[150,1400];y[0,1200];z[1e-4,10,log]'],
-                   dataFormats=['pdf']
+T2tt_1.setSources( dataLabels=['upperLimits','obsExclusion','expExclusion'],
+                   dataFiles=['orig/CMS-SUS-19-009_Figure_006.txt',
+                              'orig/CMS-SUS-19-009_Figure_006_obsExclusion.txt',
+                              'orig/CMS-SUS-19-009_Figure_006_expExclusion.txt'],
+                   units = ['pb',None,None],
+                   dataFormats=['txt','txt','txt']
 )
 
-"""
+#+++++txName block +++++++++++++++++
 T2ttoff=dataset.addTxName('T2ttoff')
 T2ttoff.checked=''
 T2ttoff.constraint="[[['b','W']],[['b','W']]]"
@@ -91,11 +91,40 @@ T2ttoff.condition=None
 T2ttoff.conditionDescription=None
 T2ttoff.massConstraint=[['80<dm<169'],['80<dm<169']]
 T2ttoff.source="CMS"
+#++++++next mass plane block+++++++++
+T2ttoff_1 = T2ttoff.addMassPlane(2*[[x,y]])
+T2ttoff_1.figure='Fig. 6'
+T2ttoff_1.figureUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_006.png'
+T2ttoff_1.dataUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_006.pdf'
+T2ttoff_1.exclusionDataUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_006.pdf'
+T2ttoff_1.setSources(dataLabels=['upperLimits','obsExclusion','expExclusion'],
+                   dataFiles=['orig/CMS-SUS-19-009_Figure_006.txt',
+                              'orig/CMS-SUS-19-009_Figure_006_obsExclusion.txt',
+                              'orig/CMS-SUS-19-009_Figure_006_expExclusion.txt'],
+                   units = ['pb',None,None],
+                   dataFormats=['txt','txt','txt'])
 
-T2tt_1.setSources(dataLabels=['expExclusion','expExclusionM1','expExclusionP1','obsExclusion','obsExclusionM1','obsExclusionP1','upperLimits','expectedUpperLimits'],
-                    dataFiles=['orig/T2ttexpectedmasslimitcurve.csv','orig/T2ttexpected-1s.d.masslimitcurve.csv','orig/T2ttexpected+1s.d.masslimitcurve.csv','orig/T2ttobservedmasslimitcurve.csv','orig/T2ttobserved-1s.d.(theory)masslimitcurve.csv','orig/T2ttobserved+1s.d.(theory)masslimitcurve.csv','orig/T2tt_obs_upperlimits.csv','orig/T2tt_exp_upperlimits.csv'],
-                    dataFormats=['csv','csv','csv','csv','csv','csv','csv','csv'],units=[None,None,None,None,None,None,'pb','pb'])
-"""
-# T2ttoff.addMassPlane(T2tt_1)
+
+#====================   T6bbWW   ==============================
+#+++++txName block +++++++++++++++++
+T6bbWW=dataset.addTxName('T6bbWW')
+T6bbWW.checked=''
+T6bbWW.constraint="[[['b'],['W']],[['b'],['W']]]"
+T6bbWW.condition=None
+T6bbWW.conditionDescription = None
+T6bbWW.source="CMS"
+#++++++next mass plane block+++++++++
+T6bbWW_1 = T6bbWW.addMassPlane(2*[[x,0.5*x+0.5*y,y]])
+T6bbWW_1.figure='Fig. 7'
+T6bbWW_1.figureUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_007.png'
+T6bbWW_1.dataUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_007.pdf'
+T6bbWW_1.exclusionDataUrl='http://cms-results.web.cern.ch/cms-results/public-results/publications/SUS-19-009/CMS-SUS-19-009_Figure_007.pdf'
+T6bbWW_1.setSources(dataLabels=['upperLimits','obsExclusion','expExclusion'],
+                   dataFiles=['orig/CMS-SUS-19-009_Figure_007.txt',
+                              'orig/CMS-SUS-19-009_Figure_007_obsExclusion.txt',
+                              'orig/CMS-SUS-19-009_Figure_007_expExclusion.txt'],
+                   units = ['pb',None,None],
+                   dataFormats=['txt','txt','txt'])
+
 
 databaseCreator.create()
